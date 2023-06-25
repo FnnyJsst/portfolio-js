201b5fd80e2dd726641ec5d99dac6ca7a7828ef4
Fanny Jousset <fannyjousset@MacBook-Air-de-Fanny.local>
2023-06-25 12:58:42 +1000

    toggle color change js

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-let h1 = document.querySelector('h1');
+document.addEventListener("DOMContentLoaded", function() {
+    let toggle = document.querySelector(".switch");
+    // let home = document.querySelector(".home");
+    // let nav = document.querySelector("nav");
+    let body = document.querySelector("html");
 
-h1.onclick = function(element) {
-    element.style.color = 'white'
-    };
+    toggle.addEventListener("click", () => {
+        document.documentElement.style.backgroundColor = "black";
+        document.documentElement.style.color = "white";
+        // home.style.backgroundColor = "black";
+        // nav.style.backgroundColor = "black";
+        body.style.backgroundColor = "black";
+    });
+});
+
